7df03fc2e43ad1ab4dcfcc5c99ac34769432ea09
SanyaGubrani <127206660+SanyaGubrani@users.noreply.github.com>
2023-08-18 19:54:59 +0530

    initial code

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const container = document.querySelector('.container');
+
+
+for(let i=0;i<16;i++){
+    for(let j=0;j<16;j++){
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        container.appendChild(div);
+
+        div.addEventListener('mouseenter', function(){
+            div.style.backgroundColor = 'black';
+        });
+    }
+}
+
+jscolor.installByClassName('jscolor');
+
